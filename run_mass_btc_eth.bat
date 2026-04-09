a8e0d5ffcab6487190b947b0c3131b0f2f76f576
0xgavrs <119516413+GAVRS1@users.noreply.github.com>
2026-04-09 22:56:12 +0300

    Delete run_mass_btc_eth.bat

--- run_mass_btc_eth.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-REM Run from repository root.
-cd /d "%~dp0"
-echo run_mass_btc_eth.bat is deprecated.
-echo Use run_project.bat and set RECOVERY_ENABLE_* / RECOVERY_CONSOLES_* in .env.
-call run_project.bat
-endlocal
